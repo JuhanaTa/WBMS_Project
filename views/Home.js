009626b47fc94b1f71cf9b41e0ceee8e66a1a90f
JuhanaTa <61382156+JuhanaTa@users.noreply.github.com>
2020-09-26 19:56:18 +0300

    first commit

---
+++ views/Home.js
@@ -0,0 +1,25 @@
+
+import React from 'react';
+import {
+  Text,
+  SafeAreaView,
+  StyleSheet,
+} from 'react-native';
+
+const Home = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>This is home</Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-between',
+  },
+});
+
+
+export default Home;
---
+++ views/Profile.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {
+  Text,
+  SafeAreaView,
+  StyleSheet,
+} from 'react-native';
+
+const Home = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>This is Profile</Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-between',
+  },
+});
+
+
+export default Home;
